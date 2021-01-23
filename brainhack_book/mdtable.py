a11a128b02201652b88740ba1e395b9c78eb4cd1
Hao-Ting Wang <htwangtw@gmail.com>
2021-01-23 18:05:02 +0000

    ENH script prepend header and paragraph to markdown table

--- brainhack_book/mdtable.py
+++ brainhack_book/mdtable.py
@@ -1,18 +1,51 @@
-from pathlib import Path
+'''
+Author: Hao-Ting Wang
+Date: 23-01-2021
 
+Generate Markdown table from csv / tsv file and
+append to an existing markdown file containing
+page header and other free text paragraph for
+acknowledgements and contributions page.
+
+Usage:
+>> python brainhack_book/mdtable.py
+
+'''
+from pathlib import Path
 import csv
+
 
 div_h = "-"
 div_v = "|"
 padding = 2
 
-
 class MarkdownTable():
-    def __init__(self, table, title):
+    '''
+    Generate and save markdown table from csv/tsv file,
+    append to an existing markdown file.
+
+    inputs
+    ------
+    table:
+        table loaded through csv.reader
+        Each row is a list and have the same lenght
+        e.g.:
+        table = [["a", "b", "c"],
+            ["d", "e", "f"],
+            ["g", "i", "j"]]
+
+    descriptions:
+        List of string where each item is a line
+    '''
+    def __init__(self, table, descriptions):
         self.table = table
-        self.title = title
+        self.descriptions = descriptions
 
     def generate(self, filename):
+        """
+        generate markdown file with a table
+        save as a .md file
+        """
         header = self.table [0]
         body = self.table [1:]
 
@@ -20,29 +53,44 @@ class MarkdownTable():
         horiz = self.header_div(header)
         header = self.add_div(header)
         body = [self.add_div(row) for row in body]
-        mdtable = self.assemble_table(self.title, header, horiz, body)
+        # write the table
+        mdtable = self.assemble_table(self.descriptions,
+            header, horiz, body)
         self.write_md(filename, mdtable)
 
     @staticmethod
     def add_div(line):
+        '''
+        add markdown table divider to a row
+        '''
         div = ''.join([padding * ' ', div_v, padding * ' '])
         return div.join(line)
 
     @staticmethod
     def header_div(header):
+        '''
+        add markdown table horizontal divider bellow header
+        '''
         col_widths = [len(cell) for cell in header]
         horizs = ["-" * w for w in col_widths]
         div = ''.join([padding * div_h, div_v, padding * div_h])
         return div.join(horizs)
 
     @staticmethod
-    def assemble_table(title, header, horiz, body):
-        table = [title, header, horiz, *body]
+    def assemble_table(title_part, header, horiz, body):
+        '''
+        assemble header, devider and table body as
+        a markdown table
+        prepend with exisiting .md file
+        '''
+        table = title_part + [header, horiz, *body]
         return [row.rstrip() for row in table]
 
     @staticmethod
     def write_md(filepath, table):
-        # write to a markdown
+        '''
+        write output from `assemble_table` to a .md file
+        '''
         md = '\n'.join(table)
         with open(filepath, "w") as f:
             f.write(md)
@@ -51,7 +99,10 @@ def parse_affliation(data):
     parsed = data[2:]  # start of header
     return parsed
 
-def read_file(filename, delimiter="\t"):
+def read_tablefile(filename, delimiter="\t"):
+    '''
+    Read tsv or csv
+    '''
     if delimiter not in [",", "\t"]:
         print(f"unsupported delimiter `{delimiter}`")
         return
@@ -60,12 +111,25 @@ def read_file(filename, delimiter="\t"):
         csv_reader = csv.reader(f, delimiter=delimiter)
         return list(csv_reader)
 
+def read_page_descriptions(filename):
+    '''
+    Read the header and addtional text from a markdown file.
+    Line breakers were stripped to fit MarkdownTable.assemble_table
+    '''
+    with open(filename, "r") as f:
+        return [l.split("\n")[0] for l in f.readlines()]
+
 
 if __name__ == '__main__':
     project_root = Path("__file__").parent
+
     ack_path = project_root / "data" / "acknowledgements.csv"
-    table = read_file(ack_path, delimiter=",")
-    mder = MarkdownTable(table, title="# Acknowledgements")
+    ack_desc_path = project_root / "data" / "acknowledgements_descriptions.md"
+
+    table = read_tablefile(ack_path, delimiter=",")
+    desc = read_page_descriptions(ack_desc_path)
+
+    mder = MarkdownTable(table, desc)
     mder.generate(project_root / "brainhack_book" / "acknowledgements.md")
 
     # aff_path = project_root / "affiliations.csv"
---
+++ data/acknowledgements_descriptions.md
@@ -0,0 +1,7 @@
+# Acknowledgements
+
+We acknowledge all the individuals who contributed and will make ongoing contributions to these resources.
+Moreover, we would like to acknowledge all Brainhack organizers, supporters, presenters, and participants for their contribution to growing and maintaining this community.
+The benefits described in this manuscript would not be possible without them.
+We also thank all institutions, labs, and organisations who have helped this community grow, meet in stimulating environments, and add an excellent educational resource pool and agenda. With an expanding community, Brainhack’s support network keeps growing and we thank all labs and individual researchers for their dedication and expertise offered to this community.
+
