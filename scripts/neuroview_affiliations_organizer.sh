52e207fa3b09f971aefaf3867e2ef761f92f6a01
Hao-Ting Wang <htwangtw@gmail.com>
2021-02-10 19:41:37 +0000

    ADD my edits
    Every author has an assoicated ID
    Affliliations are ranked based on original input order
    Remove some trailing white spaces issue

--- scripts/neuroview_affiliations_organizer.sh
+++ scripts/neuroview_affiliations_organizer.sh
@@ -1,42 +1,56 @@
 #!/bin/bash
+# Original script: Stephanie Noble Feb 09 2021
+# Edited by Hao-Ting Wang Feb 10 2021
+# Usage:
+# bash neuroview_affiliations_organizer.sh
 
 # edit filename
-orig_file='/Users/steph//Downloads/affiliation_and_consent_for_the_brainhack_neuroview_preprint_raw.tsv'
-
+orig_file="./data/affiliation_and_consent_for_the_brainhack_neuroview_preprint_raw.tsv"
+out_file="./data/affiliations_organized.tsv"
 
 cut -f10-15 "$orig_file" > tmp
 
-#reorder
-awk -v OFS="\t" -F"\t" '{print $1,$3,$2,$4,$5,$6}' tmp > tmp2
+#reorder and remove the first two lines
+awk -v OFS="\t" -F"\t" 'NR > 2 {print $1,$3,$2,$4,$5,$6}' tmp > tmp2
 
 #rename affiliation headers
-sed 's#Affiliation (please use the format: Department / Institution / City / Country)#Department/Institution/City/Country#g; s#Your 2nd affiliation (optional) - please use same format as above#Department/Institution/City/Country#g; s#Your 3rd affiliation (optional) - please use same format as above#Department/Institution/City/Country#g' tmp2 > tmp3
+sed 's#Affiliation (please use the format: Department / Institution / City / Country)#Department / Institute / City / Country#g; s#Your 2nd affiliation (optional) - please use same format as above#Department / Institute / City / Country#g; s#Your 3rd affiliation (optional) - please use same format as above#Department / Institute / City / Country#g' tmp2 > tmp3
 
 # separate 1st-3rd affils
 cut -f1-4 tmp3 > aff1
 cut -f1-3,5 tmp3 > aff2
 cut -f1-3,6 tmp3 > aff3
 
-# columns for each affiliation subfield (separately in case there are issues)
-sed 's#\/#	#g' aff1 > aff1_sep
-sed 's#\/#	#g' aff2 > aff2_tmp
-sed 's#\/#	#g' aff3 > aff3_tmp
+# remove forward slash as well as white space separate by tab explicitly
+sed 's: / :\t:g' aff1 > aff1_tmp
+sed 's: / :\t:g' aff2 > aff2_tmp
+sed 's: / :\t:g' aff3 > aff3_tmp
 
 # empty first 3 columns (names) for aff2 and aff3
-awk -v OFS="\t" -F"\t" '{$1=""; $2=""; $3=""; print ;}' aff2_tmp > aff2_sep
-awk -v OFS="\t" -F"\t" '{$1=""; $2=""; $3=""; print ;}' aff3_tmp > aff3_sep
+awk -v OFS="\t" -F"\t" '{$1=""; $2=""; $3=""; print ;}' aff2_tmp > aff2_tmp2
+awk -v OFS="\t" -F"\t" '{$1=""; $2=""; $3=""; print ;}' aff3_tmp > aff3_tmp2
 
+# HTW - add line number to start of the line (NR-1: as subject ID) and affiliation order(1; 2; 3)
+awk -v OFS="\t" -F"\t" '{print NR-1,1,$s}' aff1_tmp > aff1_sep
+awk -v OFS="\t" -F"\t" '{print NR-1,2,$s}' aff2_tmp2 > aff2_sep
+awk -v OFS="\t" -F"\t" '{print NR-1,3,$s}' aff3_tmp2 > aff3_sep
 
 # re-merge
-paste -d '\n'  aff1_sep aff2_sep aff3_sep > tmp
+paste -d '\n' aff1_sep aff2_sep aff3_sep > tmp
 
-# remove empty rows (whitespace)
-sed '/^[[:space:]]*$/d' tmp > tmp2
+# remove lines that does not contain alphabets (lines with line number and tabs only)
+sed '/[[:alpha:]]/!d' tmp > $out_file
 
-# add empty column headers
-# this seems not to like adding multiple empty columns next to each other - will have to figure it out
-awk -F"\t" '{$1=FS$1;$5=FS$5;$6=FS$6;$7=FS$7;$9=FS$9;$11=FS$11;$13=FS$13;$14=FS$14}1' OFS="\t" tmp2 > affiliations_organized.txt
+# remove line 2 and three
+sed -i '2d;3d' $out_file
 
+# replace some header labels to fit AutoArragne in line 1 (header)
+sed -i '1!b;s/0/Author_ID/' $out_file
+sed -i '1!b;s/1/Aff_Order/' $out_file
+sed -i '1!b;s/1/Aff_Order/' $out_file
+sed -i '1!b;s/First name/First/' $out_file
+sed -i '1!b;s/Middle initial(s)/Middle/' $out_file
+sed -i '1!b;s/Last name/Last/' $out_file
 
 # you might want to clean up all those tmp* and aff* files - not all my tools support editing in place and I didn't want to mv everything
-# rm tmp*; rm aff*
+rm *tmp*; rm aff?; rm aff?_sep
