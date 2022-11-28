be89bee1268868143e0670a1eec2d41d2493bfe0
Remi Gau <remi_gau@hotmail.com>
2022-11-28 22:54:35 +0100

    add authors to CFF

--- scripts/generate_citation_cff.py
+++ scripts/generate_citation_cff.py
@@ -1,16 +1,59 @@
 from pathlib import Path
 
 import pandas as pd
+from cffconvert.cli.create_citation import create_citation
+from cffconvert.cli.validate_or_write_output import validate_or_write_output
 from utils import load_citation, root_dir, write_citation
+
+
+def return_author_list_for_cff(contributors):
+
+    author_list = []
+
+    for _, row in contributors.iterrows():
+
+        given_names = row["First name"].strip()
+        if row["Middle initial(s)"] not in ["", " "]:
+            given_names += " " + row["Middle initial(s)"].strip()
+
+        author = {
+            "given-names": given_names,
+            "family-names": row["Last name"].strip(),
+            "affiliation": row["affiliation"]
+            .strip()
+            .replace(" /", ",")
+            .replace(",  , ", ", ")
+            .replace(",  ,", ""),
+        }
+        if not pd.isna(row["orcid"]):
+            author["orcid"] = f"https://orcid.org/{row['orcid'].strip()}"
+        author_list.append(author)
+
+    order = [author["family-names"] for author in author_list]
+    order = sorted(order)
+
+    author_list = sorted(author_list, key=lambda k: order.index(k["family-names"]))
+
+    return author_list
 
 
 def main():
 
-    contributors = pd.read_csv(
-        root_dir().joinpath("data", "contributors", "contributors.tsv")
-    )
+    contributors_file = root_dir().joinpath("data", "contributors", "contributors.tsv")
+    contributors = pd.read_csv(contributors_file, sep="\t")
 
     citation_file = root_dir().joinpath("CITATION.cff")
-
     citation = load_citation(citation_file)
+
+    citation["authors"] = return_author_list_for_cff(contributors)
+
     write_citation(citation_file, citation)
+
+    citation = create_citation(infile=citation_file, url=None)
+    validate_or_write_output(
+        outfile=None, outputformat=None, validate_only=True, citation=citation
+    )
+
+
+if __name__ == "__main__":
+    main()
